e8c6656c4d74ca7e90e704f9c3dc1d054608c06d
Martin Kruger <martin@martinkruger.com>
2021-02-23 19:10:47 +0200

    feat: basic node setup

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './App.style';
 import JoinGame from '../../views/JoinGame/JoinGame';
+import { io } from 'socket.io-client';
+
+const SERVER = 'http://localhost:3001';
+
+let socket: any;
 
 function App() {
+  useEffect(() => {
+    //
+    socket = io(SERVER);
+    socket.on('userConnected', (data: any) => {
+      console.log(data);
+    });
+  }, []);
+
+  function handleJoin(name: string, room: string) {
+    console.log('Handle join');
+    socket.emit('attemptJoin', { name, room });
+  }
+
   return (
     <>
-      <JoinGame />
+      <JoinGame handleJoin={handleJoin} />
     </>
   );
 }
--- client/src/views/JoinGame/JoinGame.tsx
+++ client/src/views/JoinGame/JoinGame.tsx
@@ -1,21 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './JoinGame.style';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
 import Logo from '../../assets/images/logo-white.png';
 
-function JoinGame() {
+type Props = {
+  handleJoin: (name: string, room: string) => void;
+};
+
+function JoinGame(props: Props) {
+  const { handleJoin } = props;
+
+  /** The name of the player */
+  const [playerName, setPlayerName] = useState('');
+
+  /** The room the player will join */
+  const [room, setRoom] = useState('');
+
+  /**
+   * Validates and sets the players name from an HTML input.
+   * @param event The Input element event
+   */
+  const changePlayerName = (event: React.FormEvent<HTMLInputElement>): void => {
+    const { value } = event.currentTarget as HTMLInputElement;
+    if (value.match(/^[A-Za-z0-9]+$/)) {
+      setPlayerName(value);
+    }
+  };
+
+  /**
+   * Validates and sets the room to play in from an HTML input.
+   * @param event The Input element event
+   */
+  const changeRoom = (event: React.FormEvent<HTMLInputElement>): void => {
+    const { value } = event.currentTarget as HTMLInputElement;
+    const _value = value.toUpperCase();
+    console.log(_value);
+    if (_value.match(/^[A-Za-z0-9]+$/)) {
+      setRoom(_value);
+    }
+  };
+
+  /**
+   * Submits the user's details
+   * @param event The Form event
+   */
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-    console.log('Submit');
+    handleJoin(playerName, room);
   };
 
   return (
     <S.Wrapper>
       <img src={Logo} alt="Trivia App Logo" />
       <form onSubmit={handleSubmit}>
-        <Input label="Name" required />
-        <Input label="Room" required />
+        <Input
+          label="Name"
+          name="name"
+          id="name"
+          required
+          onChange={changePlayerName}
+          value={playerName}
+        />
+        <Input
+          label="Room"
+          name="room"
+          id="room"
+          required
+          onChange={changeRoom}
+          value={room}
+        />
         <Button type="submit" invert fullWidth>
           JOIN
         </Button>
